c4724d78c6a820ca1553f8feb9bd4614a3df5cdf
Maki0419-git <yoshinomaki90530@gmail.com>
2022-04-22 17:21:57 +0800

    first commit

---
+++ domPractice.js
@@ -0,0 +1,26 @@
+// DOM manipulation
+
+// getElementById()
+
+const topElement = document.getElementById('top')
+console.log(topElement)
+
+// getElementByClassName()
+
+const rowElement = document.getElementsByClassName('row')
+console.log(rowElement)
+
+// getElementByTagName()
+
+const liElement = document.getElementsByTagName('li')
+console.log(liElement)
+
+//querySelector()
+// select the first element 
+
+const spanElement = document.querySelector('span')
+console.log(spanElement)
+
+//querySelectorAll()
+const allSpanElement = document.querySelectorAll('span')
+console.log(allSpanElement)
